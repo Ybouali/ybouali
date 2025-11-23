4d592214684dc4b9a75f7b38d08ee5326e136171
yassine bouali <yassine.bouali.bo@gmail.com>
2025-11-23 22:40:29 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="flex flex-row justify-center items-center h-screen bg-gray-900 ">
+            <h1 className="text-3xl font-bold">Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
