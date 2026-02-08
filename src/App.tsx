5048877789c5435acf634545471e093d5e605ce8
yassine bouali <yassine.bouali.bo@gmail.com>
2026-02-08 03:55:29 +0100

    add Google Analytics 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { Footer, SideBar } from './components';
 import Terminal from './components/Terminal';
 import { useAppContext } from './context/AppContext';
 import Pages from './pages';
+import { useLocation } from 'react-router-dom';
+
+ReactGA.initialize('G-CHY8D877KV');
 
 function App() {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Track page view on every route change
+        ReactGA.send({
+            hitType: 'pageview',
+            page: location.pathname + location.search,
+        });
+    }, [location]);
+
     const { openTerminal, setOpenTerminal } = useAppContext();
 
     useEffect(() => {
@@ -25,13 +39,13 @@ function App() {
     return (
         <div className="h-screen w-screen bg-[#1e1f1f] text-white overflow-hidden font-sans md:grid md:grid-cols-[250px_1fr] flex flex-col">
             <SideBar />
-            
+
             <div className="flex flex-col h-full min-w-0 relative overflow-hidden">
                 <Pages />
-                
+
                 {/* Terminal sits here, taking available space or fixed height, pushing footer down */}
                 {openTerminal && <Terminal />}
-                
+
                 <Footer />
                 <Toaster
                     position="bottom-right"
